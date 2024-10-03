65dff1b77283bda4b55846853cdb5051a3c3d5b4
Acer <Acer@LAPTOP-NOA>
2024-10-02 21:52:48 -0500

    Agregar archivos de proyecto.

---
+++ Le_inventorie/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Le_inventorie
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            // Add your login logic here
+            MessageBox.Show("Logeo Exitoso");
+        }
+
+        private void LinkRegistro_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            RegistroForm registroForm = new RegistroForm();
+            registroForm.LoginForm = this;  // Pass a reference to this form
+            registroForm.Show();
+            this.Hide();
+        }
+    }
+}
